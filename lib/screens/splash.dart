70102feb27aa94fef76958a2d5855b2b6a4f973f
azzaaasss <mobilelegendsazza@gmail.com>
2024-01-08 04:28:06 +0700

    init

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -37,7 +37,7 @@ class _SplashScreenState extends State<SplashScreen>
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              child: Image.asset("../assets/images/logo.png"),
+              child: Image.asset("assets/images/logo.png"),
             )
           ],
         ),
